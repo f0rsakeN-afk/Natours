e06af6250c608fb77969cedb305c51a62bba5a9d
Naresh Rajbanshi <nar3sh0@gmail.com>
2024-05-05 09:06:23 +0545

    post err

--- backend/server.js
+++ backend/server.js
@@ -11,44 +11,15 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+
   })
   .then((con) => {
     //console.log(con.connections);
     console.log('DB connections successful');
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
 
-const Tour = mongoose.model('tours', tourSchema);
 
-const testTour = new Tour({
-  name: 'zara',
-  rating: 8,
-  price: 6895,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR',err);
-  });
 
 const port = process.env.PORT;
 app.listen(port, () => {
