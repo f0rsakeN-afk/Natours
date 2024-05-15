23a16bff866c3eff13ce7c84f10aa8ee8f956648
Naresh Rajbanshi <nar3sh0@gmail.com>
2024-05-15 21:34:44 +0545

    query middleware remain

--- backend/model/tourModel.js
+++ backend/model/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -6,6 +7,7 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true
     },
+    slug: String,
     duration: {
         type: Number,
         required: [true, 'A tour must have a duration']
@@ -48,12 +50,53 @@ const tourSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now(),
-        select:false
+        select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    toJSON: {
+        virtuals: true
+    },
+    toObject: {
+        virtuals: true
+    }
 });
 
+
+tourSchema.virtual('durationWeeks').get(function () {
+    return this.duration / 7;
+})
+
+//this is document middleware and it runs before .save() command and .create() command
+tourSchema.pre('save', function (next) {
+    //console.log(this);
+    this.slug = slugify(this.name, { lower: true });
+    next();
+})
+
+//we can have multiple middleware for same hook
+
+
+tourSchema.post('save', function (doc, next) {
+    console.log(doc);
+    next();
+})
 const Tour = mongoose.model('tours', tourSchema);
 
 
+// query middleware allows us to run functions before and after a certain query is executed
+//this keyword will point to the query not the doc
+//QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function (next) {
+    this.find({ secretTour: { $ne: true } })
+    next();
+})
+
 module.exports = Tour;
+//Types of middlewares
+//document query aggregate model
+
