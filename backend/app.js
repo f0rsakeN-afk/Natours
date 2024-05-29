d21b3da49bf2a8a710348edbb1f28c92f522ae03
Naresh Rajbanshi <nar3sh0@gmail.com>
2024-05-29 08:11:37 +0545

    err handling for each controller remain

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -24,4 +26,14 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't fins ${req.originalUrl} on this server!`, 404))
+});
+
+
+
+app.use(globalErrorHandler);
 module.exports = app;
